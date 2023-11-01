cfea04e5785fe591bb6a67687fb74c9c3ebe60bb
a-ziz-7 <129803663+a-ziz-7@users.noreply.github.com>
2023-10-31 21:46:04 -0400

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,17 @@
+public abstract class Account {//super class for my accounts
+
+    protected Currency currency;
+
+    public Account(Currency deposit) {
+        currency = deposit;
+    }
+
+    public abstract String getType();
+
+    public abstract void withdraw(Currency money) throws Exception;
+
+    public abstract void deposit(Currency money) throws Exception;
+
+    public abstract Currency getBalance();
+
+}
